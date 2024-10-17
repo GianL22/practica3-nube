8c627ebf5b9dc000f162294abae2f711d982112c
deroldan26 <der2600@gmail.com>
2024-10-17 12:03:55 -0400

    create directory

--- src/directory/dto/create-directory.dto.ts
+++ src/directory/dto/create-directory.dto.ts
@@ -1,1 +1,10 @@
-export interface CreateDirectoryDTO {}
+import { IsUUID, IsString, MinLength, minLength, IsEmail, IsArray } from "class-validator";
+
+export class CreateDirectoryDTO {
+    @IsUUID()
+    id: string;
+    @IsString()
+    name: string;
+    @IsArray()
+    emails: string[];
+}
